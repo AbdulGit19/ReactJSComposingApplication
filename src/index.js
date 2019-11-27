c04762b2059f1550fd204cd168ba6e14e103d4c7
AbdulGit19 <58026477+AbdulGit19@users.noreply.github.com>
2019-11-27 13:54:59 +0530

    SpecialisedComponent

--- src/index.js
+++ src/index.js
@@ -5,33 +5,52 @@ import "./styles.css";
 
 import "bootstrap/dist/css/bootstrap.css";
 
-import { ActionButton } from "./ActionButton";
-import { Message } from "./Message";
-import { ThemeSelector } from "./ThemeSelector";
+//import { ActionButton } from "./ActionButton";
+//import { Message } from "./Message";
+//import { ThemeSelector } from "./ThemeSelector";
+
+import { GeneralList } from "./GeneralList";
+import { SortedList } from "./SortedList";
 
 export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      //counter: 0
+      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"]
     };
   }
 
-  incrementCounter = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
+  // incrementCounter = () => {
+  //   this.setState({ counter: this.state.counter + 1 });
+  // };
+
+  // render() {
+  //   return (
+  //     <div className="m-2 text-center">
+  //       <ThemeSelector>
+  //         <Message theme="primary" message={`Counter: ${this.state.counter}`} />
+  //         <ActionButton
+  //           theme="secondary"
+  //           text="Increment"
+  //           callback={this.incrementCounter}
+  //         />
+  //       </ThemeSelector>
+  //     </div>
+  //   );
+  // }
 
   render() {
     return (
-      <div className="m-2 text-center">
-        <ThemeSelector>
-          <Message theme="primary" message={`Counter: ${this.state.counter}`} />
-          <ActionButton
-            theme="secondary"
-            text="Increment"
-            callback={this.incrementCounter}
-          />
-        </ThemeSelector>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-6">
+            <GeneralList list={this.state.names} theme="primary" />
+          </div>
+          <div className="col-6">
+            <SortedList list={this.state.names} />
+          </div>
+        </div>
       </div>
     );
   }
