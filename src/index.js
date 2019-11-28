8646fe4cd7947be27fa546ace773eb7d0937638b
AbdulGit19 <58026477+AbdulGit19@users.noreply.github.com>
2019-11-28 16:45:49 +0530

    Render

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import "bootstrap/dist/css/bootstrap.css";
 //import { ActionButton } from "./ActionButton";
 //import { Message } from "./Message";
 //import { ThemeSelector } from "./ThemeSelector";
+import { ProFeature } from "./ProFeature";
 
 import { GeneralList } from "./GeneralList";
 import { SortedList } from "./SortedList";
@@ -17,9 +18,15 @@ export class App extends Component {
     super(props);
     this.state = {
       //counter: 0
-      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"]
+      names: ["Zoe", "Bob", "Alice", "Dora", "Joe"],
+      cities: ["London", "New York", "Paris", "Milan", "Boston"],
+      proMode: false
     };
   }
+
+  toggleProMode = () => {
+    this.setState({ proMode: !this.state.proMode });
+  };
 
   // incrementCounter = () => {
   //   this.setState({ counter: this.state.counter + 1 });
@@ -44,11 +51,28 @@ export class App extends Component {
     return (
       <div className="container-fluid">
         <div className="row">
+          <div className="col-12 text-center p-2">
+            <div className="form-check">
+              <input
+                type="checkbox"
+                className="form-check-input"
+                value={this.state.proMode}
+                onChange={this.toggleProMode}
+              />
+              <label className="form-check-label">Pro Mode</label>
+            </div>
+          </div>
+        </div>
+        <div className="row">
           <div className="col-6">
             <GeneralList list={this.state.names} theme="primary" />
           </div>
+
           <div className="col-6">
-            <SortedList list={this.state.names} />
+            <ProFeature
+              pro={this.state.proMode}
+              render={() => <SortedList list={this.state.names} />}
+            />
           </div>
         </div>
       </div>
